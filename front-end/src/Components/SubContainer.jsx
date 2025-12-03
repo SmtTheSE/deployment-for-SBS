433baa3a0b982a26ecb67be11cd2461afdba4eb1
sittMinThar005 <sitt.thar0149@sbsuni.edu.vn>
2025-12-03 15:07:41 +0700

    Fixing all things

--- front-end/src/Components/SubContainer.jsx
+++ front-end/src/Components/SubContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SubContainer = ({ children, className }) => {
   return (
-    <div className={`${className} xs:w-[95%] md:w-[80%] mx-auto`}>
+    <div className={`${className} xs:w-[95%] md:w-[100%] mx-auto`}>
       {children}
     </div>
   );
